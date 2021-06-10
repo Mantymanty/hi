ddc0094791b65febd71c550bb8c936af748bd9e0
mahan <mahan@LAPTOP-TQFQ8PS7>
2021-06-11 01:16:54 +0530

    hii

---
+++ work10/src/sample/Demo2.java
@@ -0,0 +1,17 @@
+package sample;
+
+import java.io.FileInputStream;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Demo2 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("utf");
+	}
+		
+}
